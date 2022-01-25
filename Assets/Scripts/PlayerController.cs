f65397b6c880dacc97019b7879da82d7dd6a64fe
happiey99 <50543299+happiey99@users.noreply.github.com>
2022-01-25 12:25:08 +0900

    연습
    연습1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,57 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController Cc;
-    float gravity = -20;
-    float jumpHight = 6;
-    float speed = 6f;
     
-    Vector3 velocity;
-
-    public Transform groundCheck;
-   public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    bool isGrounded;
+    
+ 
+   
 
     // Start is called before the first frame update
     void Awake()
     {
-        velocity = Vector3.zero;
-        Cc = transform.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && velocity.y < 0) 
-        {
-            velocity.y = -2f;
-        }
-
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-
-            Cc.Move(direction * speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHight * -2 * gravity);
-        }
        
-        velocity.y += gravity * Time.deltaTime; 
-        Cc.Move(velocity * Time.deltaTime);
     }
 }
