2fbb299319657be3b5a29066e923578f81ca4234
happiey99 <50543299+happiey99@users.noreply.github.com>
2022-02-14 17:04:39 +0900

    카페에서 맥북으로
    ㅂㄴㅇ

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,41 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-
-    Vector3 moveToPlayer;
     Transform realCam;
-    float yCam;
-    float xCam;
-
     GameObject player;
-    float xSpeed;
-    float ySpeed;
-    float cameraTime;
-    float cameraMoveSpeed;
 
+    private float cameraMoveSpeed;
+    private float xCam;
+    private float yCam;
 
     private void Awake()
     {
         player = GameObject.Find("Player/LookHere");
+        
         realCam = Camera.main.transform;
         transform.position = player.transform.position;
         transform.rotation = Quaternion.identity;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-    
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        xSpeed = 20f;
-        ySpeed = 20f;
-        cameraTime = 0.3f;
-        cameraMoveSpeed = 40f;
-        
+        cameraMoveSpeed = 6f;
     }
 
     // Update is called once per frame
@@ -51,27 +42,26 @@ public class CameraMove : MonoBehaviour
         BlockCamera();
     }
 
-    RaycastHit hit;
-
-    public void BlockCamera()
-    {
-    
-      
-   
-    }
-
-
     public void LookAround()
     {
-        yCam += Input.GetAxis("Mouse X") * Time.deltaTime * xSpeed;
-        xCam += -Input.GetAxis("Mouse Y") * Time.deltaTime * ySpeed;
-        xCam = Mathf.Clamp(xCam, -30, 10);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(xCam, yCam, transform.rotation.z), cameraTime * Time.deltaTime * cameraMoveSpeed);
+        xCam += Input.GetAxis("Mouse Y") ;
+        yCam += Input.GetAxis("Mouse X")  * -1;
+
+        xCam = Mathf.Clamp(xCam, -30, 30);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(xCam, yCam, 0),0.1f);
     }
 
     public void Follow()
     {
-        moveToPlayer = Vector3.Lerp(transform.position, player.transform.position, cameraTime * Time.deltaTime * cameraMoveSpeed);
-        transform.position = moveToPlayer;
+        transform.position =
+            Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * cameraMoveSpeed);
     }
+    
+    RaycastHit hit;
+    public void BlockCamera()
+    {
+
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController CC;
+
+    [SerializeField] private float Speed = 6.0f;
     
+    Vector3 PlayerCurFront;
+
+    private Vector3 curVelocity;
     
-    // Start is called before the first frame update
     void Awake()
     {
-
+        CC = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-       
+        CurFront();
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        if (!Input.anyKey)
+            return;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = new Vector3(PlayerCurFront.x,0, PlayerCurFront.z);
+        
+        CC.Move(move * Speed * Time.deltaTime); 
+    }
+
+    void CurFront()
+    {
+        PlayerCurFront = (transform.position - Camera.main.transform.position).normalized;
+        transform.forward = new Vector3(PlayerCurFront.x,0,PlayerCurFront.z);
     }
 }
