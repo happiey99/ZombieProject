2b91772b75ef25a398b3424c65562412e5937594
happiey99 <50543299+happiey99@users.noreply.github.com>
2023-08-04 19:23:43 +0930

    Camera Fix, Devide rig and cam
    1

--- Zombie/Assets/Scenes/Scripts/CameraController.cs
+++ Zombie/Assets/Scenes/Scripts/CameraController.cs
@@ -1,45 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations.Rigging;
 
 public class CameraController : MonoBehaviour
 {
-    public Rig rig;
-
-    public Transform aimTarget;
+   
     public Transform cameraTarget;
 
     float x = 0;
     float y = 0;
 
-
-    PlayerController player;
-
-    public Vector3 cameraOriPosition;
+    Vector3 cameraOriPosition;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        player = transform.parent.GetComponent<PlayerController>();
-
-
         cameraOriPosition = new Vector3(0, 0, -2);
-
     }
 
-    // Update is called once per frame
+ 
     void LateUpdate()
     {
         Camera.main.transform.LookAt(cameraTarget);
         LookAround();
         MouseWheel();
-
-        AimPositioin();
-        ClampAroundHead();
-
         CameraRayHitWall();
     }
 
@@ -54,48 +39,11 @@ public class CameraController : MonoBehaviour
         transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x + y, transform.rotation.y + x, 0));
     }
 
-    public float distance;
-
-
-    void ClampAroundHead()
-    {
-        if (transform.localEulerAngles.y <= 100 &&
-            transform.localEulerAngles.y >= -10 ||
-            transform.localEulerAngles.y <= 370 &&
-            transform.localEulerAngles.y >= 260)
-        {
-            rig.weight = Mathf.Lerp(rig.weight, 1, 0.1f);
-        }
-        else
-        {
-            rig.weight = Mathf.Lerp(rig.weight, 0, 0.1f);
-        }
-
-    }
-
-
-    void AimPositioin()
-    {
-        Vector2 midOfS = new Vector2(Screen.width / 2f, Screen.height / 2f);
-
-        Ray ray = Camera.main.ScreenPointToRay(midOfS);
-
-        RaycastHit[] hits = Physics.RaycastAll(ray);
-
-        for (int i = 0; i < hits.Length; i++)
-        {
-            if (hits[i].transform.gameObject.layer != 6)
-            {
-                aimTarget.position = hits[i].point;
-                break;
-            }
-        }
-    }
 
 
     void CameraRayHitWall()
     {
-        Vector3 CameraRayDir = (Camera.main.transform.position - cameraTarget.position).normalized;
+        Vector3 CameraRayDir = (Camera.main.transform.position - cameraTarget.position);
         float rayDis = CameraRayDir.magnitude;
         Ray ray = new Ray(cameraTarget.position, CameraRayDir);
         RaycastHit hit;
@@ -106,6 +54,7 @@ public class CameraController : MonoBehaviour
                 Vector3.Lerp(Camera.main.transform.position,
                 hit.point,
                 0.7f * Time.deltaTime * 10);
+            Debug.Log(hit.transform.gameObject.name);
         }
         else
         {
@@ -118,7 +67,6 @@ public class CameraController : MonoBehaviour
 
     float wheelVelue = -2;
 
-
     void MouseWheel()
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
@@ -129,7 +77,5 @@ public class CameraController : MonoBehaviour
 
         cameraOriPosition = new Vector3(0, 0, wheelVelue);
     }
-
-
 
 }
--- Zombie/Assets/Scenes/Scripts/Rig/RigHead.cs
+++ Zombie/Assets/Scenes/Scripts/Rig/RigHead.cs
@@ -5,6 +5,55 @@ using UnityEngine.Animations.Rigging;
 
 public class RigHead : MonoBehaviour
 {
-  
-    
+    Rig rig;
+    Transform aimTarget;
+    CameraController cameraTrans;
+
+    private void Start()
+    {
+        rig = GetComponent<Rig>();
+        aimTarget = transform.GetChild(0).GetChild(0).transform;
+        cameraTrans = transform.parent.GetComponentInChildren<CameraController>();
+    }
+    private void LateUpdate()
+    {
+        ClampAroundHead();
+        AimPositioin();
+    }
+
+    void ClampAroundHead()
+    {
+        if (cameraTrans.transform.localEulerAngles.y <= 100 &&
+            cameraTrans.transform.localEulerAngles.y >= -10 ||
+            cameraTrans.transform.localEulerAngles.y <= 370 &&
+            cameraTrans.transform.localEulerAngles.y >= 260)
+        {
+            rig.weight = Mathf.Lerp(rig.weight, 1, 0.1f);
+        }
+        else
+        {
+            rig.weight = Mathf.Lerp(rig.weight, 0, 0.1f);
+        }
+
+    }
+
+
+    void AimPositioin()
+    {
+        Vector2 midOfS = new Vector2(Screen.width / 2f, Screen.height / 2f);
+
+        Ray ray = Camera.main.ScreenPointToRay(midOfS);
+
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].transform.gameObject.layer != 6)
+            {
+                aimTarget.position = hits[i].point;
+                break;
+            }
+        }
+    }
+
 }
